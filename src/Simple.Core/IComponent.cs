2bfe58998af880adef5c8ee7a8ed735fde0a1ebb
Ed Brown <eduard.brown123@hotmail.com>
2022-11-29 18:38:08 +0000

    Added core component interface

---
+++ src/Simple.Core/IComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace Simple.Core
+{
+    public interface IComponent
+    {
+        Guid Id { get; }
+
+        Transform Transform { get; }
+        IComponent Parent { get; }
+        bool Enabled { get; }
+
+        List<IComponent> Children { get; }
+    }
+}
---
+++ src/Simple.Core/Transform.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace Simple.Core
+{
+    public class Transform
+    {
+        public Vector2 Position { get; set; } = Vector2.Zero;
+        public Vector2 Scale { get; set; } = Vector2.Zero;
+
+        public float Rotation { get; set; } = 0;
+    }
+}
