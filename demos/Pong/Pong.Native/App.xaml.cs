e2ad6d6e1cad56c4e306b0139641a117aa47dc70
Ed Brown <eduard.brown123@hotmail.com>
2022-12-02 17:37:27 +0000

    added a demo project

---
+++ demos/Pong/Pong.Native/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pong.Native
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ demos/Pong/Pong.Native/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pong.Native
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
